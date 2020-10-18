c8a72a0baae771c0abb707ccaaa9e0582114ce77
Arjo Hooimeijer <hooimeijer@gmail.com>
2020-10-18 12:58:58 +0200

    Hilight selected layer and CAPS in purple.

--- keyboards/dztech/dz65rgb/keymaps/arjoh/keymap.c
+++ keyboards/dztech/dz65rgb/keymaps/arjoh/keymap.c
@@ -11,22 +11,39 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 };
 // </TapDance>
 
+
+// Config
+const uint8_t MAIN_COLOR[3] = {0x80, 0x00, 0x80};
+
+// layers, ordering is important!
+enum layers {
+    _MAIN,
+    _MACOS,
+    _CONTROL,
+}
+
+const layers_leds_map[] = {
+    [_MAIN] = 59,
+    [_MACOS] = 60,
+    [_CONTROL] = 63,
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_65_ansi(
+    [_MAIN] = LAYOUT_65_ansi(
         TD(TD_GRV_ESC), KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_HOME,
         KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
         LT(2,KC_CAPS),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGDN,
         KC_LSFT,                 KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_END,
         KC_LCTL,        KC_LGUI, KC_LALT,                            KC_SPC,                    KC_RALT, MO(2),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
-    [1] = LAYOUT_65_ansi(
+    [_MACOS] = LAYOUT_65_ansi(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
         _______         , _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, KC_LALT, KC_LGUI,                            _______                  , _______, _______, _______, _______, _______, _______
     ),
-    [2] = LAYOUT_65_ansi(
+    [_CONTROL] = LAYOUT_65_ansi(
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_HOME,
         _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, KC_PSCR, KC_SLCK, KC_PAUS, RESET,   KC_PGUP,
         _______, RGB_SPI, RGB_SPD, _______, _______, _______, _______, _______, _______, _______, _______, _______,          EEP_RST, KC_PGDN,
@@ -35,3 +52,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 
 };
+
+
+void rgb_matrix_indicators_user(void) {
+
+    if (host_keyboard_led_state().caps_lock) {
+        rgb_matrix_set_color(30, MAIN_COLOR[0], MAIN_COLOR[1], MAIN_COLOR[2]);
+    }
+
+    // Show Selected Layer
+    rgb_matrix_set_color(layers_leds_map[biton32(layer_state)], MAIN_COLOR[0], MAIN_COLOR[1], MAIN_COLOR[2]);
+}
